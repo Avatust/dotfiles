a33196008032ea029f3654f3d65c55b22def12c9
Martin Roznovjak <martin.roznovjak@gmail.com>
2018-09-15 16:02:26 +0300

    changing modifier for i3, chaging default font, addind configuration for second monitor

---
+++ home/.config/i3/scripts/cycle_monitor_setups.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+PRIMARY=eDP1
+EXTERN=HDMI1
+
+function status {
+    xrandr | grep -E "\b$1\b (dis)?connected "
+}
+
+function is_connected {
+    echo $1 | grep -q " connected "
+}
+
+function is_active {
+    echo $1 | grep -Eq " connected [^0-9]*[0-9]+x[0-9]+"
+}
+
+function are_same {
+    # outputs the match or the whole expression if none
+    res_and_pos=$( echo $1 | sed -E "s/.* connected [^0-9]*([0-9]+x[0-9]+\+[0-9]+\+[0-9]+).*/\1/" )
+    echo $2 | grep -q " $res_and_pos "
+}
+
+primary=$(status $PRIMARY)
+extern=$(status $EXTERN)
+
+echo primary status: "$primary"
+echo extern status: "$extern"
+
+echo -n "primary active: "; (is_active "$primary") && echo true || echo false
+echo -n "extern active: "; (is_active "$extern") && echo true || echo false
+
+echo -n "primary connected: "; (is_connected "$primary") && echo true || echo false
+echo -n "extern connected: "; (is_connected "$extern") && echo true || echo false
+
+echo -n "are same: "; (are_same "$primary" "$extern") && echo true || echo false
+
+echo
+
+if is_active "$primary"
+then
+    echo primary active
+
+    if (is_connected "$extern") && ! (are_same "$primary" "$extern")
+    then
+        echo extern connected and not cloned
+        echo cloning primary and extern
+        xrandr --auto --output $PRIMARY --same-as $EXTERN
+    else
+        echo outputs are cloned or extern is not connected
+        echo turning primary off
+        xrandr --output $PRIMARY --off
+    fi
+else
+    echo primary not active
+
+    if is_connected "$extern"
+    then
+        echo extern connected
+        echo turning primary on and left of extern
+        xrandr --auto --output $EXTERN --right-of $PRIMARY
+    else
+        echo extern not connected
+        echo turning primary on
+        xrandr --auto
+    fi
+fi
+
